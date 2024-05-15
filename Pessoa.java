195b772615e50f4dd07a1954c2441a7e3d8d7cf0
antoniolucascamposdecastro <168425705+antoniolucascamposdecastro@users.noreply.github.com>
2024-05-15 10:42:37 -0300

    Create Pessoa.java

---
+++ Pessoa.java
@@ -0,0 +1,31 @@
+public class Pessoa {
+	private String nome;
+	private int idade;
+	private double altura;
+	
+	// Encapsulamento get(), set()
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
+	public double getAltura() {
+		return altura;
+	}
+	public void setNome(String novoNome) {
+		this.nome = novoNome;
+	}
+	public void setIdade(int novaIdade) {
+		this.idade = novaIdade;
+	}
+	public void setAltura(double novaAltura) {
+		this.altura = novaAltura;
+	}
+	public static void main(String[] args) {
+
+	}
+	}
