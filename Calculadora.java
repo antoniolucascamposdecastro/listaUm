5c7ac6c2734062fb06d6f7c982b82988bac2e582
antoniolucascamposdecastro <168425705+antoniolucascamposdecastro@users.noreply.github.com>
2024-05-20 08:26:12 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,12 @@
+public int somar(int a, int b) {
+		return (a + b);
+	}
+	public int subtrair(int a, int b) {
+		return (a - b);
+	}
+	public int multiplicar(int a, int b) {
+		return (a * b);
+	}
+	public int dividir(int a, int b) {
+		return (a / b);
+	}
